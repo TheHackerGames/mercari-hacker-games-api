bb6fc373e2a7c2fdf33bbefbd6857df2b734cd2f
Dan Belden <daniel.belden@mercari.com>
2018-01-18 10:58:05 +0000

    [Dev] Enabling dev mode so we can run our API in dev mode in live

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,10 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 //umask(0000);
 
 // Prevent access unless environment var SYMFONY_ENV set to dev mode
-if (getenv('SYMFONY_ENV') !== 'dev') {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
+//if (getenv('SYMFONY_ENV') !== 'dev') {
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+//}
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . '/../app/autoload.php';
