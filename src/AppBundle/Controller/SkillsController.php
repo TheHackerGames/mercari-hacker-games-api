879f1f4347c33ac818968011589dc73056ceadbc
Dan Belden <daniel.belden@mercari.com>
2018-01-18 13:46:40 +0000

    [Stub] Started creating stubs for APIs so we can assign

---
+++ src/AppBundle/Controller/SkillsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Swagger\Annotations as SWG;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SkillsController extends Controller
+{
+    public function getMilitarySkillsAction(Request $request)
+    {
+
+    }
+
+    public function addMilitarySkillsAction(Request $request)
+    {
+
+    }
+
+    public function getUserSkillsAction(Request $request)
+    {
+
+    }
+
+    public function addUserSkillAction(Request $request)
+    {
+
+    }
+
+    public function removeUserSkillAction(Request $request)
+    {
+
+    }
+}
---
+++ src/AppBundle/Controller/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Swagger\Annotations as SWG;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UsersController extends Controller
+{
+    public function getAction(Request $request)
+    {
+
+    }
+
+    public function registerAction(Request $request)
+    {
+
+    }
+}
